6940543f0086a605c6985ce6a362c56023819ecc
kobezou <649747177@qq.com>
2016-05-09 16:47:18 +0800

    add multiple station

--- app/getDataThread.py
+++ app/getDataThread.py
@@ -21,60 +21,55 @@ class WriteThread(threading.Thread):
         self.thread_stop = False
         self.app = app
         self.instruments = []
-        self.state = {'512':'Staring', '257':'Warning', '1':'Normal'}
+        self.state = {'512':'Staring', '257':'Warning', '1':'Normal', '0':'Closed'}
     #Overwrite run() method, put what you want the thread do here 
     def run(self):
-        with self.app.app_context(): 
-            try:
-                smodel = SonModel.query.all()
-                for s in smodel:
-                    smodeldata = []
-                    com = s.getComNumber()
-                    slave = s.slaveaddress
-                    sensorsNum = s.sensorsNumber * 2
-                    instrument = minimalmodbus.Instrument(com, slave)
-                    instrument.debug = True 
-                    smodeldata.append(instrument)
-                    smodeldata.append(slave)
-                    smodeldata.append(sensorsNum)
-                    self.instruments.append(smodeldata)    
-                    print s.name ,':', instrument
-            except IOError:
-                print 'com not open!'
-            else:
-                while not self.thread_stop:
-                    for instrument in self.instruments:
-                        try:
-                            data = instrument[0].read_registers(0, instrument[SENSORSNUM])
-                            #print debug log
-                            print instrument[SLAVEADDRESS],':', data
-
-                            for i, value in enumerate(data):
-                                if value != 0:
-                                    datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).filter_by(position=i).first()
-                                    if datalog is not None:
-                                        if str(value) in self.state:
-                                            datalog.sensor_state = self.state[str(value)]
-                                            datalog.updata_time = datetime.datetime.now()
-                                            db.session.add(datalog)
-                                            db.session.commit()
-                            time.sleep(self.sleeptimes)                  
-                        except IOError,e:
-                            print 'IoError'
-                            datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).all()
-                            for log in datalog:
-                                log.sensor_state ='unOpen'
-                                log.updata_time = datetime.datetime.now()
-                                db.session.add(log)
-                                db.session.commit()
-                        except ValueError,e:
-                            print 'ValueError'
-                            # datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).all()
-                            # for log in datalog:
-                            #     log.sensor_state ='Closed'
-                            #     log.updata_time = datetime.datetime.now()
-                            #     db.session.add(log) 
-                            #     db.session.commit()
+        with self.app.app_context():
+            while not self.thread_stop:
+                try:
+                    del self.instruments[:]     #empty instruments
+                    smodel = SonModel.query.all()   #acquire all sonmodel
+                    for s in smodel:
+                        smodeldata = []
+                        com = s.getComNumber()
+                        slave = s.slaveaddress
+                        sensorsNum = s.sensorsNumber * 2
+                        instrument = minimalmodbus.Instrument(com, slave)
+                        smodeldata.append(instrument)
+                        smodeldata.append(slave)
+                        smodeldata.append(sensorsNum)
+                        self.instruments.append(smodeldata)     #acquire all sonmodel's instrusment
+                except IOError:
+                    print 'com not open!'
+                else:
+                    try:
+                        for instrument in self.instruments:
+                            datas = instrument[0].read_registers(0, instrument[SENSORSNUM])
+                            pop_zero_data = datas[::2]
+                            for i, value in enumerate(pop_zero_data):
+                                datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).filter_by(position=i).first()
+                                if datalog is not None:
+                                    if str(value) in self.state:
+                                        datalog.sensor_state = self.state[str(value)]
+                                        datalog.updata_time = datetime.datetime.now()
+                                        db.session.add(datalog)
+                                        db.session.commit()
+                    except IOError:
+                        datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).all()
+                        for log in datalog:
+                            log.sensor_state ='unOpen'
+                            log.updata_time = datetime.datetime.now()
+                            db.session.add(log)
+                            db.session.commit()
+                    except ValueError:
+                        datalog = SensorLog.query.filter_by(slave_id=instrument[SLAVEADDRESS]).all()
+                        for log in datalog:
+                            log.sensor_state ='Error'
+                            log.updata_time = datetime.datetime.now()
+                            db.session.add(log) 
+                            db.session.commit() 
+                finally:
+                    time.sleep(self.sleeptimes)
             
     def stop(self):  
         self.thread_stop = True  
